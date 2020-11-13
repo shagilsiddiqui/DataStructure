567ed818b08611157731f4f209b5d773113d0367
shagilsiddiqui <40637140+shagilsiddiqui@users.noreply.github.com>
2020-11-13 15:53:34 +0530

    Add files via upload

---
+++ Reverse a linked list using recursion.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node{
+  int data;
+  struct node* link;
+};
+struct node* head=NULL;
+struct node* create(int n){
+    struct node*temp=(struct node*)malloc(sizeof(struct node));
+    temp->data=n;
+    temp->link=NULL;
+    return temp;
+}
+void insert(int n){
+    if(head==NULL){
+        head=create(n);
+        cout<<head<<endl;
+        return;
+    }
+    struct node* temp1=head;
+    head=create(n);
+    head->link=temp1;
+   // cout<<head->link<<endl;
+    
+}
+void print(){
+    struct node* temp1=head;
+    while(temp1!=NULL){
+        cout<<temp1->data<<endl;
+        temp1=temp1->link;
+    }
+}
+void Delete(int n){
+    if(n==1){
+        node* temp=head;
+        head=temp->link;
+        free(temp);
+        return;
+    }
+    struct node* temp=head;
+    for(int i=0;i<n-2;i++){
+        temp=temp->link;
+    }
+node*t=temp->link;
+    temp->link=t->link;
+    free(t);
+}
+void reverseIterative(){
+    node*temp=NULL;
+    while(head!=NULL){
+        node*temp1=head->link;
+        head->link=temp;
+        if(temp1==NULL){
+            return;
+        }
+        temp=head;
+        head=temp1;
+    }
+}
+void printRecursion(node * head){
+    if(head==NULL){
+        return;
+    }
+    cout<<head->data<<endl;
+    printRecursion(head->link);
+}
+void printReverseRecursion(node* head){
+    if(head==NULL){
+        return;
+    }
+    printReverseRecursion(head->link);
+    cout<<head->data<<endl;
+}
+void ReverseRecursion(node* p){
+    if(p->link==NULL){
+        head=p;
+        return;
+    }
+    ReverseRecursion(p->link);
+    node* temp=p->link;
+    temp->link=p;
+    p->link= NULL;
+    
+}
+int main()
+{
+    insert(5);
+    insert(10);
+    insert(15);
+    insert(20);
+    print();
+    // Delete(2);
+    // print();
+    //reverseIterative();
+    //print();
+    ReverseRecursion(head);
+    print();
+}
